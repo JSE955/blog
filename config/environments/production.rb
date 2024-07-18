7d70d6ea79a72e54e182567c8971951c7689ad88
JSE955 <jordanscottedwards@gmail.com>
2024-07-18 14:01:58 -0400

    Configure production for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.secret_key_base = Rails.application.credentials.dig(:secret_key_base)
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
